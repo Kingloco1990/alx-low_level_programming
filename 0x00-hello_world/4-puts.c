009842a5ea925144b8719bf09637f32d12ad7a1f
Kingloco1990 <ransfordwelbeck@gmail.com>
2022-04-07 12:56:53 -0700

    Commit for task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
-int main()
+/*
+ *main - this is a function and any code in its {} will be executed
+ *Return 0 - ends the main function
+ * 
+ */   
+
+int main(void)
 {
 	puts("\"Programming is like building a multiligual puzzle");
 	return (0);
