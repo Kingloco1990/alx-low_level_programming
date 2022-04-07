cdca62572d06f598fa77507d54e53dea6175db7e
Kingloco1990 <ransfordwelbeck@gmail.com>
2022-04-07 14:46:05 -0700

    Commit for task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - execution and termination of program
+ * Return: ends main function
+ */
+int main(void)
+{
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %dcl byte(s)\n", sizeof(float));
+	return (0);
+}
