c57ab953f308d78a35c3bf8ae0e4bcc60b4eb085
Kingloco1990 <ransfordwelbeck@gmail.com>
2022-04-07 15:10:44 -0700

    Commit for task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - execution and termination of program
+ * Return: ends main function
+ */
+int main(void)
+{
+	fprint(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	retun 1;
+}
