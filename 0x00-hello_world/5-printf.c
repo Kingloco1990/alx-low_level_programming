99a45438faec515a327b621223a7fd72bccd0743
Kingloco1990 <ransfordwelbeck@gmail.com>
2022-04-07 13:45:36 -0700

    Commit for task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ * main -this is a function and any code in its {} will be executed
+ *
+ * return 0 - ends the main function
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	retun (0);
+}
