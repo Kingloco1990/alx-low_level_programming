4aa7dd28a9a48edf6a2920f6b38a1afd26155b60
Kingloco1990 <ransfordwelbeck@gmail.com>
2022-04-08 09:16:29 -0700

    Commit for task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -9,12 +9,12 @@ int main(void)
 {
 	char letter;
 
-	for(letter = 'a'; letter <= 'z'; letter++)
+	for(letter = 'A'; letter <= 'Z'; letter++)
 	{
-		putchar(letter);
+		putchar(tolower(letter));
 	}
 
 	putchar("\n");
-	
+
 	return (0);
 }
