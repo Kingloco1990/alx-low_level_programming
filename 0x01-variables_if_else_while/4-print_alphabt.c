ea34da860c59005097d87cff330b3802c2e4e5ca
Kingloco1990 <ransfordwelbeck@gmail.com>
2022-04-08 13:50:19 -0700

    Commit for task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ *main - execution and termination of program
+ *
+ *Return: ends main function
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter != 'q' && letter != 'e')
+		{
+			putchar(letter);
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
