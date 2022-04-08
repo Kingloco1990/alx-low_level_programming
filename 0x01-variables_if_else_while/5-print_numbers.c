1e095fedee62c85d103f2fd502b33eb24540e53c
Kingloco1990 <ransfordwelbeck@gmail.com>
2022-04-08 14:30:35 -0700

    Commit for task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *main - execution and termination
+ *
+ *Return: ends main function
+ */
+int main(void)
+{
+	int digit;
+
+	for (digit = 0; digit < 10; digit++)
+	{
+		printf("%d", digit);
+	}
+
+	printf("\n");
+
+	return (0);
+}
