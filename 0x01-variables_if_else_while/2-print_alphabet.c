6f0c92f51370c1c5d951931ad88011bfa09fed5c
Kingloco1990 <ransfordwelbeck@gmail.com>
2022-04-08 08:28:32 -0700

    Commit for task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main - execution and termination of program
+ *
+ *Return: ends main function
+ *
+ */
+int main()
+{
+	char ch;
+
+	for(ch = "a"; ch <= "z"; ch++)
+	{
+		putchar(ch"\n");
+		putchar("\n");
+	}
+	return (0);
+}
