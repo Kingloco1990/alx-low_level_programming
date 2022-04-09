953139285d73bb8886e3ab2eefb7e395ef670697
Kingloco1990 <ransfordwelbeck@gmail.com>
2022-04-08 19:34:02 -0700

    Commit for task 11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+/**
+ *main - execution and termination of program
+ *
+ *Return: ends main function
+ */
+int main(void)
+{
+	int digit;
+	int number;
+	int figure;
+
+	for (digit = '0'; digit <= '9'; digit++)
+	{
+		for (number = '0'; number <= '9'; number++)
+		{
+			for (figure = '0'; figure <= '9'; figure++)
+			{
+				if (digit < number && number < figure)
+				{
+					putchar(digit);
+					putchar(number);
+					putchar(figure);
+
+					if (digit != '7')
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+
+		}
+	}
+	
+	putchar('\n');
+
+	return (0);
+}
