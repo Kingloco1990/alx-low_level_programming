b6eed3c4ee7d59e9c39dfad6ff03e4b0ed7bf55b
Kingloco1990 <ransfordwelbeck@gmail.com>
2022-04-08 20:37:05 -0700

    Commit for task 12

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ *main - execution and termination of program
+ *
+ *Return: ends main function
+ */
+int main(void)
+{
+	int digit;
+	int number;
+
+	for (digit = 0; digit <= 98; digit++)
+	{
+		for (number = digit + 1; number <= 99; number++)
+		{
+			putchar((digit / 10) + '0');
+			putchar((digit % 10) + '0');
+			putchar(' ');
+			putchar((number / 10) + '0');
+			putchar((number % 10) + '0');
+
+			if (digit == 98 && number == 99)
+			{
+				 continue;
+				 putchar(',');
+				 putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
