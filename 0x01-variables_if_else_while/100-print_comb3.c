d99da6744996e32832a09f566842623140fe3fda
Kingloco1990 <ransfordwelbeck@gmail.com>
2022-04-08 19:12:09 -0700

    Commit for task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ *main - execution and termination of program
+ *
+ *Return: ends main function
+ *
+ */
+int main(void)
+{
+	int digit;
+	int number;
+
+	for (digit = '0'; digit < '9'; digit++)
+	{
+		for (number = '0'; number < '9'; number++)
+		{
+			if (digit < number)
+			{
+				putchar(digit);
+				putchar(number);
+
+				if (digit != '8' || (digit == '8' && number == '9'))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+
+	return (0);
+}
