8a34fa97d0a82bb9f78dffda0175a1dea0a040c5
Kingloco1990 <ransfordwelbeck@gmail.com>
2022-04-12 12:33:56 -0700

    Commit for task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ *main - execution and termination of program (check the code)
+ *
+ *Return: ends main function (always 0)
+ */
+void print_alphabet(void);
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+
+void print_alphabet(void)
+{
+	char i;
+			
+	for (i = 'a'; i <= z; i++)
+	{
+		_putchar(i);
+	}
+
+	_putchar('\n');
+}
