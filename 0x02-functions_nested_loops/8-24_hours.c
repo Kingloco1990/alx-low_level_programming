cb97ca85c192138a759cac26904631adb575213b
Kingloco1990 <ransfordwelbeck@gmail.com>
2022-04-12 16:33:56 -0700

    Commit for task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,14 +8,14 @@
 void jack_bauer(void)
 {
 	int hours_tens, hours_ones, minutes_tens, minutes_ones, hours_max;
-	
+
 	hours_max = 58;
 	hours_tens = '0';
 	while (hours_tens < '3')
 	{
 		if (hours_tens == '2')
 		{
-			if (hours_tens == '2')
+			hours_max = '4';
 		}
 		hours_ones = '0';
 		while (hours_ones < hours_max)
@@ -37,7 +37,7 @@ void jack_bauer(void)
 				minutes_ones = '0';
 				minutes_tens++;
 			}
-		
+
 			minutes_tens = '0';
 			hours_ones++;
 		}
