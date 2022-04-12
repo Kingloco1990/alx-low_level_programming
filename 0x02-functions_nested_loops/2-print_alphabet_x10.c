429c386eb854b8d0cdcee8e0319e8a30263d13df
Kingloco1990 <ransfordwelbeck@gmail.com>
2022-04-12 13:21:14 -0700

    Commit for task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *print_alphabet_x10 - function that prints 10 times the alphabets in lowercase
+ *
+ */
+void print_alphabet_x10(void)
+{
+	char j;
+
+	int i = 0;
+
+	while (i < 10)
+	{
+		for (j = 'a'; j <= 'z'; j++)
+		{
+			putchar(j);
+		}
+		
+		putchar('\n');
+		
+		i++;
+	}
+}
