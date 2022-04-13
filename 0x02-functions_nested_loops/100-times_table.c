254d8c58bc839fe7705f1d74b10737c74cd8fd31
Kingloco1990 <ransfordwelbeck@gmail.com>
2022-04-13 02:16:28 -0700

    Commit for task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -4,40 +4,40 @@
  *                    
  * @n: The value whose times table is printed
  */
-void print_times_table(int)
+void print_times_table(int n)
 {
-	int nn, mm, pp;
+	int i, j, k;
 
 	if (n >= 0 && n <= 15)
 	{
-		for (nn = 0; nn <= n; nn++)
+		for (i = 0; i <= n; i++)
 		{
 			_putchar('0');
-			for (mm = 1; mm <= n; mm++)
+			for (j = 1; j <= n; j++)
 			{
 				_putchar(',');
 				_putchar(' ');
 
-				pp = nn * mm;
+				pp = i * j;
 
-				if (pp <= 99)
+				if (k <= 99)
 				{
 					_putchar(' ');
 				}
-				if (pp <= 9)
+				if (k <= 9)
 				{
 					_putchar(' ');
 				}
-				if (pp >= 100)
+				if (k >= 100)
 				{
-					_putchar((pp / 100) + '0');
-					_putchar(((pp / 10)) % 10 + '0');
+					_putchar((k / 100) + '0');
+					_putchar(((k / 10)) % 10 + '0');
 				}
-				else if (pp <= 99 && pp >= 10)
+				else if (k <= 99 && k >= 10)
 				{
-					_putchar((pp / 10) + '0');
+					_putchar((k / 10) + '0');
 				}
-				_putchar((pp % 10) + '0');
+				_putchar((k % 10) + '0');
 			}
 			_putchar('\n');
 		}
