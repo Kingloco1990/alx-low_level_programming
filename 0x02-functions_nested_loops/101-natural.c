a84ff8f6b910b0efa6eff587a81d8f0da53d1372
Kingloco1990 <ransfordwelbeck@gmail.com>
2022-04-13 02:25:56 -0700

    Commit for task 13

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *main - execution and termination of program
+ *
+ *Return: ends the main funtion
+ */
+int main(void)
+{
+	int i = 1024, j, sum = 0;
+	for (j = 0; j < i; j++)
+	{
+		if ((j % 3 == 0) || (j % 5 == 0))
+		{
+			sum = sum + j;
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
