ffd359610d64dd83960ae5a63a509bf668c11a59
Kingloco1990 <ransfordwelbeck@gmail.com>
2022-04-13 04:18:59 -0700

    Commit for task 15

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ *main - execution and termination of program
+ *
+ *Return: ends the main function
+ */
+int main(void)
+{
+	long int i, j = 1, k = 2, sum = 0, tSum = 0;
+
+	for (i = 0; i < 49; i++)
+	{
+		if ((k % 2 == 0) && (k <= 4000000);		
+		{
+			tSum = tSum + k;
+		}
+		sum = j + k;
+		j = k;
+		k = sum;
+	}
+	printf("%ld\n", tSum);
+	return (0);
+}
