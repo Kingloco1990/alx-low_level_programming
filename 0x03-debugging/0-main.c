da8172d4c3a365323e15616ea647792e2f2a2fbb
Kingloco1990 <ransfordwelbeck@gmail.com>
2022-04-14 07:28:54 -0700

    Commit for task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,10 +1,8 @@
 #include "main.h"
-
 /**
- * main - tests function that prints if integer is positive or negative
- * Return: 0
+ *main - tests function that prints if integer is positive or negative
+ *Return: ends the main funciton
  */
-
 int main(void)
 {
 	int i;
