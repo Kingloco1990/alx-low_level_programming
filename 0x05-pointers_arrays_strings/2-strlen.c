d880e6af05035931b0e773f2dccde65ffe50dc73
Kingloco1990 <ransfordwelbeck@gmail.com>
2022-04-20 15:50:51 -0700

    Commit for task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *
+ * _strlen - function that computes the lenght of a string
+ * @i: variable for string length
+ * Return: shows the length of a string
+ */
+
+int _strlen(char *i)
+{
+	int a;
+	int b = 0;
+
+	for (a = 0; s[a] != '\0'; a++)
+	{
+		a++;
+	}
+
+	return (b);
+}
