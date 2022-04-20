6d0945de2fbdc2d698ba4d29f1e1b6f59c22d5a7
Kingloco1990 <ransfordwelbeck@gmail.com>
2022-04-20 16:18:23 -0700

    Commit for task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_rev - function that prints a string in reverse
+ *
+ * @i: a string
+ *
+ */
+
+void print_rev(char *i)
+{
+	int a = 0;
+	int b;
+
+	for (b = 0; i[b] != '\0'; b++)
+	{
+	}
+	
+	for (a = b - 1; a >= 0; a--)
+	{
+		_putchar(i[a]);
+	}
+
+	_putchar('\n');
+}
