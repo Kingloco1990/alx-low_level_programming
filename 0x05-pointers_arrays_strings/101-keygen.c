7781507437006cffc15181aa57c5025a20499272
Kingloco1990 <ransfordwelbeck@gmail.com>
2022-04-20 17:09:42 -0700

    Commit for task 11

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+
+/**
+ * main - execution and termination of code
+ *
+ * Return: ends the main funtion
+ *
+ */
+
+int main(void)
+{
+	int ascii = 2772, k = 0, b, random;
+	char password[100];
+	time_t t;
+
+	srand((int) time(&t));
+	while (ascii > 126)
+	{
+		random = rand() % 126;
+		password[k] = random;
+		ascii -= random;
+		k++;
+	}
+
+	if (ascii > 0)
+	{
+		password[k] = ascii;
+	}
+	else
+	{
+		k--;
+	}
+
+	for (b = 0; b <= k; b++)
+	{
+		printf("%c", password[b]);
+	}
+
+	return (0));
+}
