e47bfed47f25c95e677e538222684a5ed03db479
Kingloco1990 <ransfordwelbeck@gmail.com>
2022-04-21 09:04:39 -0700

    Commit for task 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
 /**
- * _strlen - function that computes the lenght of a string
+ * _strlen - returns the length of a string
  *
- * @i: variable for string length
- * Return: shows the length of a string
+ * @s: string whose length is to be returned
+ * Return: length of a string
  */
 
-int _strlen(char *i)
+int _strlen(char *s)
 {
-	int a;
+	int i;
 	int str_len = 0;
 
-	for (a = 0; i[a] != '\0'; a++)
+	for (a = 0; s[a] != '\0'; a++)
 	{
 		str_len++;
 	}
