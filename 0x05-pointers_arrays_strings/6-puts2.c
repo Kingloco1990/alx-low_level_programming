ff02fea9c3caae94de7169be60c3df1c08a6b90a
Kingloco1990 <ransfordwelbeck@gmail.com>
2022-04-22 09:02:26 -0700

    Commit for task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -3,8 +3,8 @@
 /**
  * puts2 - prints every other character of a string from the 1st character
  *
- * @str: a string
- *
+ * @str: string to be printed
+ * Return: nothing
  */
 
 void puts2(char *str)
