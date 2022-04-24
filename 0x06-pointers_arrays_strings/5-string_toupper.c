078c7b5bf65f3f3574243a016b9d99d4f087abe2
Kingloco1990 <ransfordwelbeck@gmail.com>
2022-04-24 12:07:32 -0700

    Commit for task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+ #include "main.h"
+
+/**
+ * string_toupper - converts lowercase string to uppercase
+ * @s: string 
+ * Return: uppercase string
+ */
+
+char *string_toupper(char *s)
+{
+	int a = 0;
+
+	while (s[a] != '\0')
+	{
+		if (s[a] > 96 && s[a] < 123)
+		{
+			s[a] -= 32;
+		}
+		a++;
+	}
+
+	return (s);
+}
