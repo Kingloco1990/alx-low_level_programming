2cabc4fb8d2859834a2123f1bf12fdf3492cb81d
Kingloco1990 <ransfordwelbeck@gmail.com>
2022-04-28 15:12:28 -0700

    Commit for task 6

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * prime2 - returns 1 if prime and 0 if otherwise
+ *
+ * @x: same number as n
+ * @y: number that iterates from 1 to n
+ * Return: integer
+ */
+int prime2(int x, int y)
+{
+	if (x == y)
+		return (1);
+	else if (x % y == 0)
+		return (0);
+	return (prime2(x, y + 1));
+}
+/**
+ * is_prime_number - checks if a number is prime
+ * @n: integer
+ *
+ * Return: integer
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	return (prime2(n, 2));
+}
