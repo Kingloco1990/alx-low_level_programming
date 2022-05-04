16fa8719f1e98bb2a70297ee5b47cbab6c857e59
Kingloco1990 <ransfordwelbeck@gmail.com>
2022-05-04 09:20:29 -0700

    Commit for task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,13 +3,14 @@
 
 /**
  * main - prints the name of the program followed by a new line
+ *
  * @argc: number of arguments passed to the program
  * @argv: arguments (array of strings) passed to the program
- * Return: noting (always zero)
+ * Return: nothing (0)
  */
 int main(int argc, char *argv[])
 {
-	if (argc > 0)
+	if (argc == 1)
 	{
 		printf("%s\n", argv[0]);
 	}
