1ffdc6f2e8f4d175c52476db539d278a1e29fb11
Kingloco1990 <ransfordwelbeck@gmail.com>
2022-05-04 12:36:17 -0700

    Commit for task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - prints the number of arguments passed into it
+ *
+ * @argc: number of arguments passed to the program
+ * @argv: arguments (array of strings) passed to the program
+ * Return: noting (always zero)
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
