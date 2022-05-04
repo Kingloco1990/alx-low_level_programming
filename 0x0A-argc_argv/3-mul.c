4dd4a5ce8bc273c71de6c3154a9e151a4360dd9d
Kingloco1990 <ransfordwelbeck@gmail.com>
2022-05-04 12:54:34 -0700

    Commit for task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiplies two numbers 
+ *
+ * @argc: number of arguments passed to the program
+ * @argv: arguments (array of strings) passed to the program
+ * Return: Nil
+ */
+
+int main(int argc, char **argv)
+{
+	int n, k;
+
+	k = 0;
+	if (argc != 3)
+	{
+		printf("%s\n", "Error");
+		k = 1;
+	}
+	else
+	{
+		n = atoi(argv[1]) * atoi(argv[2]);
+		printf("%i\n", n);
+	}
+	return (k);
+}
