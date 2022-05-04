fba89d42d3fb6204889e8fbae7eb775c254a71ec
Kingloco1990 <ransfordwelbeck@gmail.com>
2022-05-04 09:15:39 -0700

    Commit for task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 #include "main.h"
 
-/**main - prints the name of the program followed by a new line
- *
- *argc: argument count
- *argc: argument vector
- *Return: noting (always zero)
+/**
+ * main - prints the name of the program followed by a new line
+ * @argc: number of arguments passed to the program
+ * @argv: arguments (array of strings) passed to the program
+ * Return: noting (always zero)
  */
 int main(int argc, char *argv[])
 {
+	if (argc > 0)
+	{
+		printf("%s\n", argv[0]);
+	}
 
-	printf("%s\n", argv[argc * 0]);
 	return (0);
 }
