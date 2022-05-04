e35e0fe6783e01a7c513153d973f9ba067b95767
Kingloco1990 <ransfordwelbeck@gmail.com>
2022-05-04 09:04:05 -0700

    Commit for task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**main - prints the name of the program followed by a new line
+ *
+ *argc: argument count
+ *argc: argument vector
+ *Return: noting (always zero)
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s", argv[0]);
+	return (0);
+}
