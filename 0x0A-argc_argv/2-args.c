a50bee268416e74e26fc35f335db5db80a81dd45
Kingloco1990 <ransfordwelbeck@gmail.com>
2022-05-04 12:48:07 -0700

    Commit for task 2

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "main.h"
 
 /**
- * main - prints all arguments it receives 
+ * main - prints all arguments it receives
  *
  * @argc: number of arguments passed to the program
  * @argv: arguments (array of strings) passed to the program
