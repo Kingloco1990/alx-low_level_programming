398f4bfa2e2b1f36be4e61e33da92e02a598f62a
Kingloco1990 <ransfordwelbeck@gmail.com>
2022-05-04 12:45:20 -0700

    Commit for task 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints all arguments it receives 
+ *
+ * @argc: number of arguments passed to the program
+ * @argv: arguments (array of strings) passed to the program
+ * Return: nothing (0)
+ */
+
+int main(int argc, char **argv)
+{
+	int k;
+
+	if (argc > 0)
+	{
+		for (k = 0; k < argc; k++)
+		{
+			printf("%s\n", argv[k]);
+		}
+	}
+	return (0);
+}
