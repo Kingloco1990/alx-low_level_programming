041dd6acb1f10c5f1bc19f7108a209f529899194
Kingloco1990 <ransfordwelbeck@gmail.com>
2022-05-11 12:09:06 -0700

    Commit for task 2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - execution and termainion of the program
+ *
+ * Return: ends the main function (always 0)
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
