0aa1fe1a5b5d5914f1110d2bb08e1a94bd6ebd06
Kingloco1990 <ransfordwelbeck@gmail.com>
2022-05-13 10:15:52 -0700

    Commit for task 4

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -11,37 +11,36 @@
 
 int main(int argc, char *argv[])
 {
-  int bytes, index;
-  int (*address)(int, char **) = main;
-  unsigned char opcode;
+	int bytes, index;
+	int (*address)(int, char **) = main;
+	unsigned char opcode;
 
-  if (argc != 2)
-    {
-      printf("Error\n");
-      exit(1);
-    }
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
 
-  bytes = atoi(argv[1]);
+	bytes = atoi(argv[1]);
 
-  if (bytes < 0)
-    {
-      printf("Error\n");
-      exit(2);
-    }
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	for (index = 0; index < bytes; index++)
+	{
+		opcode = *(unsigned char *)address;
+		printf("%.2x", opcode);
 
-  for (index = 0; index < bytes; index++)
-    {
-      opcode = *(unsigned char *)address;
-      printf("%.2x", opcode);
+		if (index == bytes - 1)
+			continue;
+		printf(" ");
 
-      if (index == bytes - 1)
-	continue;
-      printf(" ");
+		address++;
+	}
 
-      address++;
-    }
+	printf("\n");
 
-  printf("\n");
-
-  return (0);
+	return (0);
 }
