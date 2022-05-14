68640c1686fbe8a55495763007868612e0c258ea
Kingloco1990 <ransfordwelbeck@gmail.com>
2022-05-14 04:38:02 -0700

    Commit for task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,39 +8,42 @@
 
 int main(void)
 {
-    int i;
-    int j;
-    int k;
-    int l;
+	int i;
+	int j;
+	int k;
+	int l;
 
-    for (i = 48; i <= 57; i++)
-    {
-        for (j = 48; j <= 56; j++)
-        {
-            for (k = 48; k <= 57; k++)
-            {
-                for (l = 49; l <= 57; l++)
-                {
-
-                    if ((i == k) && (j == l))
-                    {
-                        continue;
-                    }
-                    putchar(i);
-                    putchar(j);
-                    putchar(' ');
-                    putchar(k);
-                    putchar(l);
-                    if ((i == 57) && (j == 56) && (k == 57) && (l == 57))
-                    {
-                        break;
-                    }
-                    putchar(',');
-                    putchar(' ');
-                }
-            }
-        }
-    }
-    putchar('\n');
-    return (0);
+	for (i = 48; i <= 57; i++)
+	{
+		for (j = 48; j <= 56; j++)
+		{
+			for (k = 48; k <= 57; k++)
+			{
+				for (l = 49; l <= 57; l++)
+				{
+					if ((i == 48) && (j == 48) && (k == 48) && (1 == 48))
+					{
+						continue;
+					}
+					if ((i == k) && (j == l))
+					{
+						continue;
+					}
+					putchar(i);
+					putchar(j);
+					putchar(' ');
+					putchar(k);
+					putchar(l);
+					if ((i == 57) && (j == 56) && (k == 57) && (l == 57))
+					{
+						break;
+					}
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
