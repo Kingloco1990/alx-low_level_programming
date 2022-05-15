4a0ae6c92f1bb17e506b6f24909ac12b9d05ce28
Kingloco1990 <ransfordwelbeck@gmail.com>
2022-05-15 02:54:26 -0700

    Commit for task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
+
 /**
  *main - execution and termination of program
  *
  *Return: ends main function
- *
  */
+
 int main(void)
 {
 	int i, j;
 
-	for (i = '0'; i <= '9'; i++)
+	for (i = '0'; i <= '8'; i++)
 	{
 		for (j = '0'; j <= '9'; j++)
 		{
-			if (i < j && i != '9')
+			if (i < j)
 			{
 				putchar(i);
 				putchar(j);
@@ -28,8 +29,6 @@ int main(void)
 			}
 		}
 	}
-
 	putchar('\n');
-
 	return (0);
 }
