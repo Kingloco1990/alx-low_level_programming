22b0fbc09f97279ff5ab82ae30e8bef3067205ae
Kingloco1990 <ransfordwelbeck@gmail.com>
2022-05-30 21:55:27 -0700

    Commit for task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,25 +10,25 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    ssize_t file, fread, fwrite;
-    char *num_lets;
+	ssize_t file, fread, fwrite;
+	char *num_lets;
 
-    num_lets = malloc(sizeof(char) * letters);
-    if (num_lets == NULL)
-        return (0);
-    if (filename == NULL)
-        return (0);
+	num_lets = malloc(sizeof(char) * letters);
+	if (num_lets == NULL)
+		return (0);
+	if (filename == NULL)
+		return (0);
 
-    file = open(filename, O_RDONLY);
-    if (file == -1)
-        return (0);
-    fread = read(file, num_lets, letters);
-    if (fread == -1)
-        return (0);
-    fwrite = write(STDOUT_FILENO, num_lets, fread);
-    if (fwrite == -1)
-        return (0);
-    close(file);
-    free(num_lets);
-    return (fwrite);
+	file = open(filename, O_RDONLY);
+	if (file == -1)
+		return (0);
+	fread = read(file, num_lets, letters);
+	if (fread == -1)
+		return (0);
+	fwrite = write(STDOUT_FILENO, num_lets, fread);
+	if (fwrite == -1)
+		return (0);
+	close(file);
+	free(num_lets);
+	return (fwrite);
 }
