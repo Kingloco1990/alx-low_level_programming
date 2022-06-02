9908ad42e48fefa5d58f7e59e2dfb374adf85640
Kingloco1990 <ransfordwelbeck@gmail.com>
2022-06-01 20:33:01 -0700

    Commit for task 4 and 5

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -2,25 +2,26 @@
 
 /**
  * print_binary - prints the binary representation of a number
+ *
  * @n: number to print in binary
  */
+
 void print_binary(unsigned long int n)
 {
-    int k, num = 0;
-    unsigned long int first;
+	int k, num = 0;
+	unsigned long int first;
 
-    for (k = 63; k >= 0; k--)
-    {
-        first = n >> k;
-
-        if (first & 1)
-        {
-            _putchar('1');
-            num++;
-        }
-        else if (num)
-            _putchar('0');
-    }
-    if (!num)
-        _putchar('0');
+	for (k = 63; k >= 0; k--)
+	{
+		first = n >> k;
+		if (first & 1)
+	{
+		_putchar('1');
+		num++;
+	}
+	else if (num)
+		_putchar('0');
+	}
+	if (!num)
+		_putchar('0');
 }
