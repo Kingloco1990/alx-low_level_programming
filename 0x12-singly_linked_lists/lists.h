5838d693cb41f3428a2a36bf167d755e453b90a4
Kingloco1990 <ransfordnawelbeck@gmail.com>
2023-03-13 21:55:26 -0700

    Header file for the project

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,6 @@
 #ifndef _LIST_H_
 #define _LIST_H_
+
 #include <stdio.h>
 #include <stddef.h>
 #include <stdlib.h>
@@ -30,4 +31,4 @@ list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
-#endif
+#endif /*#define _LIST_H_*/
