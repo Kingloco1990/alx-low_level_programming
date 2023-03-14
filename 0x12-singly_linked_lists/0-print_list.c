288c3a164511f9cdcfd9513da50c19e973b4260c
Kingloco1990 <ransfordnawelbeck@gmail.com>
2023-03-14 03:20:48 -0700

    Prints all the elements of a list_t list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -17,8 +17,9 @@ size_t print_list(const list_t *h)
 			printf("[0] (nil)\n");
 		else
 			printf("[%d] %s\n", h->len, h->str);
-			num_nodes++;
-			h = h->next; /*updates the h pointer to point to the next node in the list*/
+
+		num_nodes++;
+		h = h->next; /*updates the h pointer to point to the next node in the list*/
 	}
 
 	return (num_nodes);
