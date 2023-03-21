55513269197b43806b3a9b6270099c471cd4d646
Kingloco1990 <ransfordnawelbeck@gmail.com>
2023-03-21 03:03:38 -0700

    Adds a new node at the beginning of a list_t list

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -17,10 +17,11 @@ list_t *add_node(list_t **head, const char *str)
 
 	/*Allocates memory on the heap for data of type list_t*/
 	new_node = malloc(sizeof(list_t));
-	if new_node == NULL
+	if (new_node == NULL)
 		return (NULL);
 
 	s = strdup(str);
+
 	if (s == NULL)
 	{
 		free(new_node);
