fdf476920df93e4d1a4644934aa633194f19fb46
Kingloco1990 <ransfordnawelbeck@gmail.com>
2023-03-23 10:11:43 -0700

    Prints a string before main() is executed

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,13 +1,15 @@
-#include <stdio.h>
+#include "lists.h"
 
-void printfirst(void) __attribute__ ((constructor));
+void print_before(void)__attribute__((constructor));
+
 /**
- * printfirst - print before main
+ * print_before - Prints a string before the main fucntion
+ *                is executed.
  *
- * Return: void
+ * Return: Nothing.
  */
-void printfirst(void)
+void print_before(void)
 {
-	printf("You're beat! and yet, you must allow,\n");
-	printf("I bore my house upon my back!\n");
+	printf(You're beat! and yet, you must allow,\n
+			I bore my house upon my back!\n);
 }
