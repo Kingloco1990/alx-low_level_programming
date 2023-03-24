a44b7b3c4a3465ea26ab96f11c021397ab303c31
Kingloco1990 <ransfordnawelbeck@gmail.com>
2023-03-24 05:32:09 -0700

    A 64-bit program in assembly that prints Hello, Holberton

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,9 +1,16 @@
-	message db "Hello, Holberton",10
-section .text
-global main
-main:
+; Purpose: To print a string using printf
+
+extern printf	; The C function to be called
+
+section.text	; Text section (for actual code)
+	global _start ; Standard gcc entry point
+
+_start:		; program label for entry point
 	mov rax, 1
-	mov rdi, 1
-	mov rsi, message
-	mov rdx, 17
+	mov rdi, 1	; File descriptor
+	mov rsi, msg	; Buffer
+	mov rdx, 17	; Length of buffer
 	syscall
+
+section.data	; Data section
+	msg db "Hello, Holberton", 10
