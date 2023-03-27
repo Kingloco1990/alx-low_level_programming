62efc1b2dcd228a3a4ff0ad89f66a83173f4f055
Kingloco1990 <ransfordnawelbeck@gmail.com>
2023-03-27 06:31:23 -0700

    Frees a listint_t list

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -17,12 +17,12 @@ void free_listint2(listint_t **head)
 		return;
 	}
 
-	while ((*head)->next != NULL)
+	while (*hea)
 	{
 		temp = (*head)->next;
 		free(*head);
 		*head = temp;
 	}
-	free(*head);
+
 	*head = NULL;
 }
