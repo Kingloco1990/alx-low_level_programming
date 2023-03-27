94154c0e961a0e4df43a8a33bbe78d77155bd585
Kingloco1990 <ransfordnawelbeck@gmail.com>
2023-03-27 09:51:40 -0700

    Inserts a new node at a given position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,63 +1,44 @@
 #include "lists.h"
-listint_t *create_new_node(int n);
 
 /**
- * insert_nodeint_at_index - inserts a new node at a given position
- * @head: Pointer to the first element of the list
- * @idx: Index to insert a node
- * @n: number to be inserted into the node
- * Return: The address of the new node
+ * insert_nodeint_at_index - Inserts a new node into a 
+ *                           listint_t list at a given 
+ *                           position.
+ * @head: A pointer to a pointer that points to the first
+ *        node of the listint_t list.
+ * @idx: The index of the listint_t list where the new
+ *       node should be added - (it starts from 0).
+ * @n: The integer to insert into the new node.
+ *
+ * Return: The address of the new node, or NULL 
+ *         if the function fails.
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	unsigned int k;
-	listint_t *j;
-	listint_t *jj;
-	listint_t *kk;
+	unsigned int i = 0, j;
+	listint_t *temp, *hold, *new;
 
-	j = *head;
-	if (head == NULL)
-		return (NULL);
-	kk = create_new_node(n);
-	if (kk == NULL)
-		return (NULL);
-	if (*head == NULL)
+	temp = *head;
+	j = idx - 1;
+
+	while (i < j)
 	{
-		*head = kk;
-		return (kk);
+		temp = temp->next;
+		i++;
 	}
 
-	if (idx == 0)
-		*head = kk;
-	for (k = 0; k < idx - 1 && j != NULL && idx != 0; k++)
-		j = j->next;
-	if (j == NULL)
-		return (NULL);
-	if (idx == 0)
-		kk->next = j;
-	else
+	hold = temp;
+	hold = temp->next;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
 	{
-		jj = j->next;
-		j->next = kk;
-		kk->next = jj;
-	}
-	return (kk);
-}
-
-/**
- * create_new_node - Creates a new node
- * @n: Value to add to new node
- * Return: A pointer to a node
- */
-listint_t *create_new_node(int n)
-{
-	listint_t *kk;
-
-	kk = malloc(sizeof(listint_t));
-	if (kk == NULL)
 		return (NULL);
-	kk->n = n;
-	kk->next = NULL;
+	}
 
-	return (kk);
-}
+	new->n = n;
+	new->next = hold;
+	temp->next = new;
+
+	return (new);
+}	
