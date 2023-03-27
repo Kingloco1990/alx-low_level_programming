c9df56e945a32b298a5b3228fcc35eb96dac9c2e
Kingloco1990 <ransfordnawelbeck@gmail.com>
2023-03-27 09:56:22 -0700

    Inserts a new node at a given position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -18,6 +18,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int i = 0, j;
 	listint_t *temp, *hold, *new;
 
+	if (idx == 0)
+	{
+		return (NULL);
+	}
+
 	temp = *head;
 	j = idx - 1;
 
