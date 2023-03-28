9cbe727e793ba499cd1bdbd15ae87ca24601d16d
Kingloco1990 <ransfordnawelbeck@gmail.com>
2023-03-28 07:58:07 -0700

    Prints all the elements of a listint_t list

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+/**
+ * print_listint_safe - Prints a listint_t linked list.
+ * @head: A pointer to the first node of the listint_t to check.
+ *
+ * Return: The number of nodes in the list.
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t len = 0;
+	const listint_t *temp;
+
+	if (head == NULL)
+	{
+		exit (98);
+	}
+
+	while (head)
+	{
+		printf("[%p] %d\n", (void *)head, head->n);
+		len++;
+		temp = head;
+		head = head->next;
+
+		if (temp <= head)
+		{
+			 printf("-> [%p] %d\n", (void *)head, head->n);
+			 exit(98);
+		}
+	}
+
+	return (len);
+}
