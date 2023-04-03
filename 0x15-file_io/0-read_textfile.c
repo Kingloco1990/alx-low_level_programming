d22fd14110e4e1be874b6e7ec3bad0462c8634e8
Kingloco1990 <ransfordnawelbeck@gmail.com>
2023-04-03 13:22:37 -0700

    Reads a text file and prints it to the POSIX standard output

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -17,11 +17,16 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t file, fread, fwrite;
 	char *num_lets;
 
+	if (filename == NULL)
+	{
+		return (0);
+	}
+
 	num_lets = malloc(sizeof(char) * letters);
 	if (num_lets == NULL)
+	{
 		return (0);
-	if (filename == NULL)
-		return (0);
+	}
 
 	file = open(filename, O_RDONLY);
 	if (file == -1)
