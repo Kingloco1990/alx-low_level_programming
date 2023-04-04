34ce0cb883ae920daea35bde4e0a2295f8c715e3
Kingloco1990 <ransfordnawelbeck@gmail.com>
2023-04-03 21:57:23 -0700

    Creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,6 +19,10 @@ int create_file(const char *filename, char *text_content)
 	}
 
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if (fd == -1)
+	{
+		return (-1);
+	}
 
 	if (text_content)
 	{
@@ -29,7 +33,7 @@ int create_file(const char *filename, char *text_content)
 
 		fwrite = write(fd, text_content, len);
 
-		if (fd == -1 || fwrite == -1)
+		if (fwrite == -1)
 		{
 			return (0);
 		}
