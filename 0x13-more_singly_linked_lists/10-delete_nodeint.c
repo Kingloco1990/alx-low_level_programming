e951de344756c3e069053e675bd00a18598feddc
Kingloco1990 <ransfordnawelbeck@gmail.com>
2023-08-09 14:41:55 +0000

    Delete the node at an index of a listint_t linked list

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -13,7 +13,6 @@
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	listint_t *temp, *hold;
-	unsigned int i = 0;
 
 	temp = *head;
 
@@ -29,7 +28,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		return (1);
 	}
 
-	while (i < (index - 1))
+	while (index > 1)
 	{
 		if (temp->next == NULL)
 		{
@@ -37,7 +36,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		}
 
 		temp = temp->next;
-		i++;
+		index--;
 	}
 
 	hold = temp->next;
